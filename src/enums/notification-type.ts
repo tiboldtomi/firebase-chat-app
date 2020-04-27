200bcb19106f189e33f4751b7d9a667377131a1c
Tamás Tibold <tiboldtomi94@gmail.com>
2020-04-27 18:41:01 +0200

    Some refact

---
+++ src/enums/notification-type.ts
@@ -0,0 +1,8 @@
+enum NotificationType {
+    Danger = 'Danger',
+    Warning = 'Warning',
+    Success = 'Success',
+};
+
+export default NotificationType;
+
---
+++ src/interfaces/index.ts
@@ -0,0 +1,3 @@
+export * from './notification.interface';
+export * from './loader-banner.interface';
+export { default as IAction } from './action.interface';
